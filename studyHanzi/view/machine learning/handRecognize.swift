7943e3a2b6bfa4f4e7a8fbbc14b31065ebde7521
nxhong93 <nxhong93@gmail.com>
2024-12-23 22:51:44 +0700

    update drawing character

---
+++ studyHanzi/view/machine learning/handRecognize.swift
@@ -0,0 +1,8 @@
+//
+//  handRecognize.swift
+//  studyHanzi
+//
+//  Created by nguyen xuan hong on 17/12/24.
+//
+
+import Foundation
---
+++ studyHanzi/view/translate/searchModel.swift
@@ -0,0 +1,8 @@
+//
+//  searchModel.swift
+//  studyHanzi
+//
+//  Created by nguyen xuan hong on 15/12/24.
+//
+
+import Foundation
