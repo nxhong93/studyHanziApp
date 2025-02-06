d8836f1a2288fb16d09f8fbbd852fb9570b8fd5a
nxhong93 <nxhong93@gmail.com>
2025-02-06 23:34:22 +0700

    update tts api

---
+++ studyHanzi/view/ai/tts.swift
@@ -0,0 +1,8 @@
+//
+//  tts.swift
+//  studyHanzi
+//
+//  Created by nguyen xuan hong on 4/2/25.
+//
+
+import Foundation
---
+++ studyHanzi/view/model/ttsParam.swift
@@ -0,0 +1,8 @@
+//
+//  ttsParam.swift
+//  studyHanzi
+//
+//  Created by nguyen xuan hong on 6/2/25.
+//
+
+import Foundation
